3674888acea752f03fd0594de620dace1833b466
emillipereira <98535441+emillipereira@users.noreply.github.com>
2022-01-31 13:48:24 -0300

    Add files via upload

---
+++ Integrado com firebase/js/init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCfIe2vIkz2VVO-5Em9w53OmlhuubyUvSQ",
+  authDomain: "weblist-fa6a6.firebaseapp.com",
+  projectId: "weblist-fa6a6",
+  storageBucket: "weblist-fa6a6.appspot.com",
+  messagingSenderId: "38441414992",
+  appId: "1:38441414992:web:6e65c579dc4a8c640d7c55"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
